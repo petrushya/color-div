c472e4998861add8fae3150e4c10dcdf5c73ba79
petrushya <108490184+petrushya@users.noreply.github.com>
2024-03-19 19:50:07 +0300

    changes the logic of inserting blocks at the end to create a line from the required number of blocks and inserting lines at the end until a square is created, adds a button to get a number and creates a grid size paragraph

--- js/main.js
+++ js/main.js
@@ -1,19 +1,60 @@
  
-const allContent = document.querySelector('.allContent');
-let divQty;
+const squareContent = document.querySelector('.squareContent');
+const dataInput = document.querySelector('.dataInput');
+const numberInput = document.querySelector('#numberInput');
+const par = document.createElement('p');
 
-if(allContent.offsetHeight < allContent.offsetWidth){
-  divQty = Math.floor(allContent.offsetHeight / 16);
+let squareSize;
+if(squareContent.offsetHeight < squareContent.offsetWidth){
+  squareSize = Math.floor(squareContent.offsetHeight/10)*10;
 }else{
-  divQty = Math.floor(allContent.offsetWidth / 16);
+  squareSize = Math.floor(squareContent.offsetWidth/10)*10;
+};
+
+squareContent.style.inlineSize = `${squareSize}px`;
+squareContent.style.blockSize = `${squareSize}px`;
+
+let divisor;
+
+function setGrid(youNumber) {
+  for (let index = 0; index < youNumber; index++){
+    const line = document.createElement('div');
+    line.setAttribute('class', 'lineaggr');
+    line.setAttribute('style', `block-size:${squareSize/youNumber}px;`);
+    for (let index = 0; index < divisor; index++) {
+      const lineFiller = document.createElement('div');
+      lineFiller.setAttribute('class', 'aggregator');
+      lineFiller.setAttribute('style', `inline-size:${squareSize/youNumber}px;block-size:${squareSize/youNumber}px;`);
+      line.appendChild(lineFiller);
+    };
+    squareContent.appendChild(line);
+  };
 }
 
-allContent.style.inlineSize = `${divQty * 16}px`;
-allContent.style.blockSize = `${divQty * 16}px`;
+if(!divisor){
+  divisor = 16;
 
-for (let index = 0; index < 256; index++) {
-  const divFiller = document.createElement('div');
-  divFiller.setAttribute('class', 'aggregator');
-  divFiller.setAttribute('style', `inline-size:${divQty}px;block-size:${divQty}px;flex:0 0 ${divQty}px;`);
-  allContent.appendChild(divFiller);
-}
+  setGrid(divisor);
+};
+
+numberInput.addEventListener('click', () => {
+  divisor=0;
+  const inputNumber = prompt('Enter a number from 16 to 100 inclusive');
+  
+  if(divisor + +inputNumber === +inputNumber && +inputNumber >=16 && +inputNumber <=100){
+    divisor = Math.floor(+inputNumber);
+    const removeDiv = squareContent.querySelectorAll('.lineaggr');
+    removeDiv.forEach((item)=>{
+      squareContent.removeChild(item);
+    });
+    par.textContent = `Grid ${divisor} X ${divisor}`;
+
+    setGrid(divisor);
+
+  }else if(+inputNumber < 16 || +inputNumber > 100){
+    par.textContent = 'The number is not in the required range! Enter again correctly';
+  }else{
+    par.textContent = 'The number must be entered in digits! Enter again correctly';
+  };
+  dataInput.appendChild(par);
+});
